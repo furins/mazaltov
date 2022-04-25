9ec95fe0054506579bc7f840c57735aa4536ea65
Stefano Furin <furins@gmail.com>
2022-04-25 18:59:29 +0200

    fix responsive YT video

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -9,7 +9,7 @@ const YoutubeEmbed = ({ embedId, title }) => (
             src={`https://www.youtube.com/embed/${embedId}?controls=0`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
+            allowFullScreen=""
             title={title}
         />
     </div>
